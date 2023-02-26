664688deed31c3e491684ae2b9e986be306ce4c9
feninTolia <fenintolja@gmail.com>
2023-02-26 13:45:48 +0200

    theme setup

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -1,5 +1,47 @@
-function Navbar() {
+import { useState } from 'react';
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Typography,
+  Select,
+  MenuItem,
+  FormControl,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
+import {
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notification,
+  Help,
+  Menu,
+  Close,
+} from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'state';
+import { useNavigate } from 'react-router-dom';
+import FlexBetween from 'components/FlexBetween';
+
+const Navbar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
+
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
+  const fullName = `${user.firstName} ${user.lastName}`;
+
   return <div>Navbar</div>;
-}
+};
 
 export default Navbar;
