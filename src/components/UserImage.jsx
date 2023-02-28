a8873dd214bf0d266a42a38bcf0a3d5341653e05
feninTolia <fenintolja@gmail.com>
2023-02-28 08:17:57 +0200

    widget with user info

---
+++ src/components/UserImage.jsx
@@ -0,0 +1,17 @@
+import { Box } from '@mui/material';
+
+const UserImage = ({ image, size = '60px' }) => {
+  return (
+    <Box width={size} height={size}>
+      <img
+        src={`http://localhost:3001/assets/${image}`}
+        alt="User"
+        style={{ objectFit: 'cover', borderRadius: '50%' }}
+        width={size}
+        height={size}
+      />
+    </Box>
+  );
+};
+
+export default UserImage;
