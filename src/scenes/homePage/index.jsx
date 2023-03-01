660ef7fb3d0ffec59cafe2a4a4f7f6d564b90438
feninTolia <fenintolja@gmail.com>
2023-03-01 10:19:16 +0200

    posts widget

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -2,6 +2,8 @@ import { Box, useMediaQuery } from '@mui/material';
 import { useSelector } from 'react-redux';
 import Navbar from 'scenes/navbar';
 import UserWidget from 'scenes/widgets/UserWidget';
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from 'scenes/widgets/PostsWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -24,7 +26,8 @@ const HomePage = () => {
           flexBasis={isNonMobileScreens ? '42%' : undefined}
           mt={isNonMobileScreens ? undefined : '2rem'}
         >
-          Posts
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
         </Box>
         {isNonMobileScreens && <Box flexBasis={'26%'}>Ads</Box>}
       </Box>
